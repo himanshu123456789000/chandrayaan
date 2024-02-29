5e9a0420e6796a860cde4ab1792c2f389038c419
Himanshu <kalehimanshu5@gmail.com>
2024-02-29 16:46:18 +0530

    moved chandrayaan_rotation methods to seperate module

--- chandrayaan.rb
+++ chandrayaan.rb
@@ -61,24 +61,4 @@ class Chandrayaan
       raise ArgumentError, "Invalid direction: #{direction}"
     end
   end
-
-  def turn_left(direction, prev_direction)
-    direction = prev_direction if prev_direction != nil && (direction == 'Up' ||direction == 'Down')
-    case direction
-    when 'N' then 'W'
-    when 'S' then 'E'
-    when 'E' then 'N'
-    when 'W' then 'S'
-    end
-  end
-
-  def turn_right(direction, prev_direction)
-    direction = prev_direction if prev_direction != nil && (direction == 'Up' ||direction == 'Down')
-    case direction
-    when 'N' then 'E'
-    when 'S' then 'W'
-    when 'E' then 'S'
-    when 'W' then 'N'
-    end
-  end
 end
